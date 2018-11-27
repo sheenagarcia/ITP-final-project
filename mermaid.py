bc5d9176de9acb24e947ce357bda6d832862e018
sheenagarcia <43712679+sheenagarcia@users.noreply.github.com>
2018-11-27 11:48:03 -0500

    Add files via upload
    changed movement so that it's not based on rect but on centre position = allows me to adjust movement speed in settings

--- mermaid.py
+++ mermaid.py
@@ -4,6 +4,8 @@ class Mermaid():
     def __init__(self, screen):
         """"Initialize mermaid and set its starting position"""
         self.screen = screen
+        #control speed via Settings
+        self.ai_settings = ai_settings
 
         #Load mermaid image (image must be bmp format)
         self.image = pygame.image.load('images/STF_mermaid.bmp')
@@ -15,6 +17,9 @@ class Mermaid():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        #I used a float since I want the value to be a decimal number
+        self.center = float(self.rect.centerx)
+
         #movement flag; set to false initially
         self.moving_right = False
         self.moving_left = False
@@ -22,10 +27,14 @@ class Mermaid():
     #however, we add the update function based on the mvt flag that will move the mermaid +1 to the right if True
     def update(self):
         """Update mermaid position based on mvt flag"""
+        #I changed it to make the update based on mermaid's center value and not the rect
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor
+
+        #*then* I can update the rect from self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Draw the mermaid at its current location"""
--- settings.py
+++ settings.py
@@ -5,3 +5,6 @@ class Settings():
         self.screen_width(1200)
         self.screen_height(800)
         self.bg_colour = (198, 180, 160)
+
+    #mermaid Settings
+    self.mermaid_speed_factor = 1.5
